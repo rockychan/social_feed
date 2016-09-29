1cf8d5a8cb088cb17dcc42275ae6d2ca37501720
YinYin Chiu <cychiuae@connect.ust.hk>
2016-09-29 14:12:36 +0800

    Create record index in after save hook

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -1,12 +1,14 @@
 import json
 import os
 import skygear
-from skygear import op
+from skygear import (
+    after_save,
+    op,
+)
 from skygear.container import SkygearContainer
 from skygear.options import options
 from skygear.utils import db
 import sqlalchemy as sa
-import uuid
 
 SKYGEAR_APP_NAME = os.getenv('APP_NAME', 'my_skygear_app')
 SOCIAL_FEED_TABLE_PREFIX = 'skygear_social_feed'
@@ -248,3 +250,68 @@ def social_feed_query_my_friends_records(serializedSkygearQuery):
             'record:query',
             serializedSkygearQuery
         )
+
+
+def add_record_to_index(record_type):
+    def after_save_add_record_to_index(record, original_record, db):
+        if original_record is not None:
+            return
+
+        record_id = record.id.key
+        record_owner_id = record.owner_id
+
+        table_name = table_name_for_relation_index(
+            prefix=SOCIAL_FEED_TABLE_PREFIX,
+            relation='friends',
+            record_type=record_type
+        )
+
+        create_index_sql = sa.text('''
+            INSERT INTO {db_name}.{table_name} (
+                _id,
+                _database_id,
+                _owner_id,
+                _created_at,
+                _created_by,
+                _updated_at,
+                _updated_by,
+                _access,
+                left_id,
+                right_id,
+                record_ref
+            )
+            SELECT
+                uuid_generate_v4() as _id,
+                '' as _database_id,
+                f1.left_id as _owner_id,
+                current_timestamp as _created_at,
+                f1.left_id as _created_by,
+                current_timestamp as _updated_at,
+                f1.left_id as _created_by,
+                '[]'::jsonb as _access,
+                f1.left_id as left_id,
+                :record_owner_id as right_id,
+                :record_id as record_ref
+            FROM {db_name}._friend f1
+            JOIN {db_name}._friend f2
+            ON f1.right_id = f2.left_id
+            WHERE f1.right_id = :record_owner_id
+            AND f2.left_id = :record_owner_id
+            AND f1.left_id = f2.right_id
+        '''.format(
+            db_name=DB_NAME,
+            table_name=table_name,
+            record_type=record_type
+        ))
+
+        db.execute(
+            create_index_sql,
+            record_owner_id=record_owner_id,
+            record_id=record_id
+        )
+
+    return after_save_add_record_to_index
+
+for record_type in SOCIAL_FEED_RECORD_TYPES:
+    after_save_add_record_to_index = add_record_to_index(record_type)
+    after_save(record_type, async=True)(after_save_add_record_to_index)
